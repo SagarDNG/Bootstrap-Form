a2ca10d5c4af376a228e8dab28417626e6944b1c
SagarDNG <sagardng123@gmail.com>
2022-06-09 00:36:55 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const firstName = document.getElementById('first-name');
+const LI = document.getElementById('1');
+
+LI.addEventListener('mouseover', () => {
+    LI.innerHTML = `First Name is`;
+})
