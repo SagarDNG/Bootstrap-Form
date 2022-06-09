1f9b9b962967f2dfc8726dd90d450cd83300af8f
SagarDNG <sagardng123@gmail.com>
2022-06-09 10:15:33 +0530

    deleted

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 
-const firstName = document.getElementById('first-name');
-const LI = document.getElementById('1');
+const button = document.getElementById('button');
+const formData = document.getElementById('formData');
 
-LI.addEventListener('mouseover', () => {
-    LI.innerHTML = `First Name is`;
+const li1 = document.getElementById('1');
+const li2 = document.getElementById('2');
+const li3 = document.getElementById('3');
+const li4 = document.getElementById('4');
+
+button.addEventListener('click', () => {
+    console.log('hi');
+    li1.innerHTML = `First Name is ${firstName.value}`;
+    li2.innerHTML = `Last Name is ${lastName.value}`;
+    li3.innerHTML = `Email is ${email.value}`;
+    li4.innerHTML = `Country is ${country.value}`;
+    formData.classList.add('d-block');
 })
