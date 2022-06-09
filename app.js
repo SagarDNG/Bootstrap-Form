9a74c5d893ce2592afe99a052fbef872ad8ec8fe
SagarDNG <sagardng123@gmail.com>
2022-06-09 11:16:08 +0530

    resolved error

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const li3 = document.getElementById('3');
 const li4 = document.getElementById('4');
 
 button.addEventListener('click', () => {
-    li1.innerText = `First Name is ${firstName.value}`;
-    li2.innerText = `Last Name is ${lastName.value}`;
-    li3.innerText = `Email is ${email.value}`;
-    li4.innerText = `Country is ${country.value}`;
+    li1.innerText = `First Name : ${firstName.value}`;
+    li2.innerText = `Last Name : ${lastName.value}`;
+    li3.innerText = `Email : ${email.value}`;
+    li4.innerText = `Country : ${country.value}`;
     formData.classList.add('d-block');
 })
