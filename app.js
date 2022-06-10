125ffb38e4b30d2ad34a6bacf85036288d8e00ae
SagarDNG <sagardng123@gmail.com>
2022-06-10 16:14:46 +0530

    bug fix

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const li2 = document.getElementById('2');
 const li3 = document.getElementById('3');
 const li4 = document.getElementById('4');
 
-button.addEventListener('click', () => {
-    li1.innerText = `First Name : ${firstName.value}`;
-    li2.innerText = `Last Name : ${lastName.value}`;
-    li3.innerText = `Email : ${email.value}`;
-    li4.innerText = `Country : ${country.value}`;
-    formData.classList.add('d-block');
-})
+button.addEventListener('click', display)
+
+function display() {
+    if (!firstName.value || !lastName.value || !email.value || !country.value) {
+        alert("Please don't click the button without filling necessary details")
+    } else {
+        li1.innerText = `First Name : ${firstName.value}`;
+        li2.innerText = `Last Name : ${lastName.value}`;
+        li3.innerText = `Email : ${email.value}`;
+        li4.innerText = `Country : ${country.value}`;
+        formData.classList.add('d-block');
+    }
+}
